9babca18a5a8293aa62b4ab0cf12aa84836766ff
ChideraEbenebe <142051938+ChideraEbenebe@users.noreply.github.com>
2023-12-11 20:19:38 +0100

    Add files via upload

---
+++ Random Quote Generator/script.js
@@ -0,0 +1,16 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const btn = document.getElementById('btn');
+
+const url = 'https://api.quotable.io/random';
+
+const getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+};
+window.addEventListener('load', getQuote);
+btn.addEventListener('click', getQuote);
